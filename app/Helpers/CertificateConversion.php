ef694fc90956adc6d7da62d7437528a4730f1c48
Marco "Mint" Introini <mint.dev@pm.me>
2025-02-18 17:35:23 +0100

    update

--- app/Helpers/CertificateConversion.php
+++ app/Helpers/CertificateConversion.php
@@ -7,15 +7,17 @@ class CertificateConversion
     public static function der2pem($der_data): string
     {
         $pem = chunk_split(base64_encode($der_data), 64, "\n");
+
         return "-----BEGIN CERTIFICATE-----\n".$pem."-----END CERTIFICATE-----\n";
     }
 
     public static function pem2der($pem_data): string
     {
-        $begin = "CERTIFICATE-----";
-        $end = "-----END";
+        $begin = 'CERTIFICATE-----';
+        $end = '-----END';
         $pem_data = substr($pem_data, strpos($pem_data, $begin) + strlen($begin));
         $pem_data = substr($pem_data, 0, strpos($pem_data, $end));
+
         return base64_decode($pem_data);
     }
 }
--- app/Helpers/CertificateFile.php
+++ app/Helpers/CertificateFile.php
@@ -3,33 +3,35 @@
 namespace App\Helpers;
 
 use App\Enums\CertTypeEnum;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
 class CertificateFile
 {
-
     private string $filePath;
+
     private CertTypeEnum $certType = CertTypeEnum::UNKNOWN;
 
     public function __construct(string $filePath)
     {
         $this->filePath = $filePath;
         $upperExtension = Str::upper(File::extension($this->filePath));
-        if (($upperExtension === "PEM")
-            || ($upperExtension === "CER")
-            || ($upperExtension === "CRT")) {
+        if (($upperExtension === 'PEM')
+            || ($upperExtension === 'CER')
+            || ($upperExtension === 'CRT')) {
             if (Str::contains(
                 haystack: File::get($this->filePath),
-                needles: "BEGIN CERTIFICATE",
+                needles: 'BEGIN CERTIFICATE',
                 ignoreCase: true
             )) {
                 $this->certType = CertTypeEnum::PEM;
+
                 return;
             }
         }
-        if ($upperExtension === "DER") {
+        if ($upperExtension === 'DER') {
             $this->certType = CertTypeEnum::DER;
+
             return;
         }
     }
@@ -38,5 +40,4 @@ class CertificateFile
     {
         return $this->certType;
     }
-
 }
